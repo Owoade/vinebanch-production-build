77edc35dd443eeeca3dfdfb47c3f60e90231f77a
Owoade <63651250+Owoade@users.noreply.github.com>
2021-09-22 23:31:18 +0100

    Update [id].jsx

--- pages/updates/[id].jsx
+++ pages/updates/[id].jsx
@@ -11,7 +11,7 @@ export const getStaticPaths = async ()=>{
  
     return {
         paths,
-        fallback:false
+        fallback:"blocking"
     }
  }
  export const getStaticProps= async (context)=>{
@@ -19,6 +19,7 @@ export const getStaticPaths = async ()=>{
      const res = await fetch(`https://vb-backend.herokuapp.com/fetch-post/${id}`);
      const data = await res.json();
      return {
+          revalidate: 60,
          props: { blog: data }
      }
  }
